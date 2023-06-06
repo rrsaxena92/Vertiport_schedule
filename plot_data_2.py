684e4fac0268308caf7f6e56444dfbfd7c0a0042
rrsaxena92 <rrsaxena92@gmail.com>
2023-06-06 12:34:23 +0530

    topo file order change
    plot script for boxplots

---
+++ plot_data_2.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+plt.rcParams['pdf.fonttype'] = 42
+plt.rcParams['ps.fonttype'] = 42
+
+figFolder = "E:/Digital_Twin_and_UAM/Figures"
+
+folder = "E:/Digital_Twin_and_UAM/ATFM_code/Vertiport_schedule/misc/"
+filename = "ArrivalsOnlyNoXtopo1"
+ext = ".csv"
+
+vertdf_dir = pd.read_csv(folder + "/" + filename + ext)
+
+ht = 5; asp = 2
+g = sns.catplot(data=vertdf_dir, x="Num_directions", y="Delay", hue="Num_flights", kind="box", showmeans=True, meanprops={"marker": ".",
+                       "markeredgecolor": "black","markersize": "5"}, height=ht, aspect=asp)
+
+g.set(yticks=range(0,int(max(vertdf_dir["Delay"])+5),10))
+g.set_ylabels("Delay (s)")
+g.set_xlabels("Number of Surface Directions")
+g._legend.set_title("Number of Flights")
+plt.tight_layout()
+plt.grid(axis='y')
+sns.move_legend(g, "upper right")
+
+plt.show()
