46f9a57d0bc9efd8c50e77607cf3a06a7e5a7d7d
rrsaxena92 <rrsaxena92@gmail.com>
2023-12-15 16:18:09 +0530

    Adding single Gate topology

---
+++ topo_0_arr_dep_dir_2.m
@@ -0,0 +1,37 @@
+Nodes.gates = {'G1'};
+Nodes.taxi  = {'b','c'};
+Nodes.TLOF  = {'R2'};
+Nodes.OVF   = {'X'};
+Nodes.dir   = {'N','E'};
+Nodes.gateC = {'G1c0','G1c1'};
+Nodes.all   = [[Nodes.gates], [Nodes.taxi], [Nodes.TLOF], [Nodes.gateC], [Nodes.OVF], [Nodes.dir]];
+
+gateCapacity = 1;
+
+Edges.gate = {'G1-b','b-G1'};
+Edges.taxi = {'b-c', 'c-b'};
+Edges.TLOF = {'c-R2', 'R2-c'};
+Edges.OVF  = {'R2-X', 'X-R2'};
+Edges.dir  = {'X-N', 'N-X','X-E','E-X'};
+Edges.all  = [[Edges.gate], [Edges.taxi], [Edges.TLOF], [Edges.OVF], [Edges.dir]];
+
+flight_path_nodes_arr={
+{'N','X','R2','c','b','G1'};
+{'E','X','R2','c','b','G1'};
+};
+
+flight_path_nodes_dep={
+{'G1','b','c','R2','X','N'};
+{'G1','b','c','R2','X','E'};
+};
+
+flight_path_edges_dep={
+{'G1-b','b-c','c-R2','R2-X','X-N'};
+{'G1-b','b-c','c-R2','R2-X','X-E'};
+};
+
+
+flight_path_edges_arr={
+{'N-X','X-R2','R2-c','c-b','b-G1'};
+{'E-X','X-R2','R2-c','c-b','b-G1'};
+};
---
+++ topo_0_arr_dep_dir_4.m
@@ -0,0 +1,45 @@
+Nodes.gates = {'G1'};
+Nodes.taxi  = {'b','c'};
+Nodes.TLOF  = {'R2'};
+Nodes.OVF   = {'X'};
+Nodes.dir   = {'N','E', 'W', 'S'};
+Nodes.gateC = {'G1c0','G1c1'};
+Nodes.all   = [[Nodes.gates], [Nodes.taxi], [Nodes.TLOF], [Nodes.gateC], [Nodes.OVF], [Nodes.dir]];
+
+gateCapacity = 1;
+
+Edges.gate = {'G1-b','b-G1'};
+Edges.taxi = {'b-c', 'c-b'};
+Edges.TLOF = {'c-R2', 'R2-c'};
+Edges.OVF  = {'R2-X', 'X-R2'};
+Edges.dir  = {'X-N', 'N-X','X-E','E-X', 'X-W','W-X','X-S','S-X'};
+Edges.all  = [[Edges.gate], [Edges.taxi], [Edges.TLOF], [Edges.OVF], [Edges.dir]];
+
+flight_path_nodes_arr={
+{'N','X','R2','c','b','G1'};
+{'E','X','R2','c','b','G1'};
+{'W','X','R2','c','b','G1'};
+{'S','X','R2','c','b','G1'};
+};
+
+flight_path_nodes_dep={
+{'G1','b','c','R2','X','N'};
+{'G1','b','c','R2','X','E'};
+{'G1','b','c','R2','X','W'};
+{'G1','b','c','R2','X','S'};
+};
+
+flight_path_edges_dep={
+{'G1-b','b-c','c-R2','R2-X','X-N'};
+{'G1-b','b-c','c-R2','R2-X','X-E'};
+{'G1-b','b-c','c-R2','R2-X','X-W'};
+{'G1-b','b-c','c-R2','R2-X','X-S'};
+};
+
+
+flight_path_edges_arr={
+{'N-X','X-R2','R2-c','c-b','b-G1'};
+{'E-X','X-R2','R2-c','c-b','b-G1'};
+{'W-X','X-R2','R2-c','c-b','b-G1'};
+{'S-X','X-R2','R2-c','c-b','b-G1'};
+};
